283ce6ee03641c57ebf114142acb6126e77cc270
Verchenko Artem <artem.avert@gmail.com>
2025-03-01 18:21:01 +0300

    8 commit. Remake main.c

--- src/main.c
+++ src/main.c
@@ -3,39 +3,67 @@
 #include <unistd.h>
 #include "users.h"
 #include "processes.h"
-#include "log.h"
-#include "errors.h"
 #include "help.h"
 
 int main(int argc, char *argv[]) {
     int opt;
     const char *log_path = NULL;
     const char *errors_path = NULL;
+    int users_flag = 0;
+    int processes_flag = 0;
 
+    // Обработка аргументов командной строки
     while ((opt = getopt(argc, argv, "uphl:e:")) != -1) {
         switch (opt) {
             case 'u':
-                print_users();
+                users_flag = 1; // Устанавливаем флаг для вывода пользователей
                 break;
             case 'p':
-                print_processes();
+                processes_flag = 1; // Устанавливаем флаг для вывода процессов
                 break;
             case 'h':
-                print_help();
+                print_help(); // Вывод справки
                 return 0;
             case 'l':
-                log_path = optarg;
-                redirect_output(log_path);
+                log_path = optarg; // Сохраняем путь для логов
                 break;
             case 'e':
-                errors_path = optarg;
-                redirect_errors(errors_path);
+                errors_path = optarg; // Сохраняем путь для ошибок
                 break;
             default:
                 fprintf(stderr, "Unknown option: %c\n", opt);
                 print_help();
-                return 1;
+                return EXIT_FAILURE;
         }
+    }
+
+    // Перенаправляем stdout в файл, если указан флаг -l
+    if (log_path) {
+        if (freopen(log_path, "w", stdout) == NULL) {
+            perror("freopen");
+            return EXIT_FAILURE;
+        }
+    }
+
+    // Перенаправляем stderr в файл, если указан флаг -e
+    if (errors_path) {
+        if (freopen(errors_path, "w", stderr) == NULL) {
+            perror("freopen");
+            return EXIT_FAILURE;
+        }
+    }
+
+    // Выполняем команды в зависимости от флагов
+    if (users_flag) {
+        print_users(); // Вывод списка пользователей
+    }
+    if (processes_flag) {
+        print_processes(); // Вывод списка процессов
+    }
+
+    // Если не было указано ни одной команды, выводим справку
+    if (optind == 1 && !users_flag && !processes_flag) {
+        print_help();
     }
 
     return 0;
